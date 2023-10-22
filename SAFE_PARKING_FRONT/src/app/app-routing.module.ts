40b9d486a7bc0b3d4d2b165df2c2f1afc87f6ffe
Jhon Harold Sikos Astete <haroldsikos@gmail.com>
2023-10-22 11:28:18 -0500

    Front_prueba_1

--- SAFE_PARKING_FRONT/src/app/app-routing.module.ts
+++ SAFE_PARKING_FRONT/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { HomeArrendadorComponent } from './components/home-arrendador/home-arrendador.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home-arrendador', component:HomeArrendadorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ SAFE_PARKING_FRONT/src/app/components/home-arrendador/home-arrendador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-arrendador',
+  templateUrl: './home-arrendador.component.html',
+  styleUrls: ['./home-arrendador.component.css']
+})
+export class HomeArrendadorComponent {  
+  ngOnInit(): void {}
+
+}

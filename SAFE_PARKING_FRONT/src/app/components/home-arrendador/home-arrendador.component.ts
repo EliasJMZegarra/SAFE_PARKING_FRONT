d5742ce80c31486423a4bc83ab074c6f19016c61
DSR124 <246810diegosr@gmail.com>
2023-10-23 17:22:24 -0500

    Modificacion de los servicios de Vehiculos

--- SAFE_PARKING_FRONT/src/app/components/home-arrendador/home-arrendador.component.ts
+++ SAFE_PARKING_FRONT/src/app/components/home-arrendador/home-arrendador.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router, RouterEvent } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-arrendador',
   templateUrl: './home-arrendador.component.html',
-  styleUrls: ['./home-arrendador.component.css']
+  styleUrls: ['./home-arrendador.component.css'],
 })
 export class HomeArrendadorComponent {
-   constructor(public route: ActivatedRoute, private router: Router) {}
+  constructor(public route: ActivatedRoute) {}
   ngOnInit() {}
 }

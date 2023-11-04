4077bf73487172671c2b0c597c35787b6fbc9cf1
DSR124 <246810diegosr@gmail.com>
2023-11-03 21:07:47 -0500

    implementacion del mservices de horario estacionamiento

--- SAFE_PARKING_FRONT/src/app/models/pago.ts
+++ SAFE_PARKING_FRONT/src/app/models/pago.ts
@@ -1,9 +1,8 @@
 import { ReservaEstacionamiento } from './reservaEstacionamiento';
-export class Membresia {
-    idPago: number = 0;
-    fechaEmision: Date = new Date(Date.now());
-    precioTotal: number = 0;
-    tipoPago: string = '';
-    reservaEstacionamiento: ReservaEstacionamiento = new ReservaEstacionamiento()
-  }
-  
+export class pago {
+  idPago: number = 0;
+  fechaEmision: Date = new Date(Date.now());
+  precioTotal: number = 0;
+  tipoPago: string = '';
+  reservaEstacionamiento: ReservaEstacionamiento = new ReservaEstacionamiento();
+}
--- SAFE_PARKING_FRONT/src/app/services/horario-estacionamiento.service.ts
+++ SAFE_PARKING_FRONT/src/app/services/horario-estacionamiento.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HorarioEstacionamiento } from '../models/horarioEstacionamiento';
+import { environment } from 'src/environments/environment';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+const base_url = environment.base_datos; // ruta de la base de datos
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HorarioEstacionamientoService {
+  private url = `${base_url}/horarioEstacionamientos`;
+  private listaCambio = new Subject<HorarioEstacionamiento[]>();
+  constructor(private http: HttpClient) {}
+  // Obtener todos los Horarios
+  list() {
+    return this.http.get<HorarioEstacionamiento[]>(`${this.url}/Listar`);
+  }
+  setList(listaNueva: HorarioEstacionamiento[]) {
+    this.listaCambio.next(listaNueva);
+  }
 
-  constructor() { }
+  getList() {
+    return this.listaCambio.asObservable();
+  }
+  // Obtener un Horario por ID
+  listId(id: number) {
+    return this.http.get<HorarioEstacionamiento>(
+      `${this.url}/ListarporID/${id}`
+    );
+  }
+  // Actualizar un Horario
+  update(hE: HorarioEstacionamiento) {
+    return this.http.put(`${this.url}/Modificar`, hE);
+  }
+  // Eliminar un Horario
+  delete(id: number) {
+    return this.http.delete(`${this.url}/Eliminar/${id}`);
+  }
+  // Crear un nuevo Horario
+  insert(lc: HorarioEstacionamiento) {
+    return this.http.post(`${this.url}/Registrar`, lc);
+  }
 }
